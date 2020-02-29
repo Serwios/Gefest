3a28f46e994683313c90b5c2e4f299853b6e5a81
Serwio YK <kucelas@ukr.net>
2020-02-29 10:58:18 +0200

    Finish

--- JDBC/src/main/java/first_stack/com.mysql.cj.jdbc.Driver.java
+++ JDBC/src/main/java/first_stack/com.mysql.cj.jdbc.Driver.java
@@ -1,17 +1,18 @@
 package first_stack;
 
 import java.sql.*;
+import java.util.Scanner;
 
 class first_stack{
 
-    static final String DATABASE_URL = "jdbc:mysql://new:3306/new?useLegacyDatetimeCode=false&amp&serverTimezone=UTC";
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/new?useLegacyDatetimeCode=false&amp&serverTimezone=UTC";
 
     static final String USER = "root";
     static final String PASSWORD = "root";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Connection connection = null;
-        Statement statement = null;
+        Connection connection;
+        Statement statement;
 
         System.out.println("Registering JDBC driver...");
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -27,26 +28,65 @@ class first_stack{
 
         ResultSet resultSet = statement.executeQuery(sql);
 
-        System.out.println("\nMovies:");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a film: ");
+        String film = in.nextLine();
+
         while (resultSet.next()) {
-            int id = resultSet.getInt("ID");
-            String name = resultSet.getString("Name");
+
+            String names = resultSet.getString("Name");
             String Director = resultSet.getString("Director");
             int year = resultSet.getInt("Year");
             String Country = resultSet.getString("Country");
 
-            System.out.println("\n================\n");
-            System.out.println("id: " + id);
-            System.out.println("Name: " + name);
-            System.out.println("Director: " + Director);
-            System.out.println("Year: " + year);
-            System.out.println("Country: " + Country);
+             if(film.equals(names)){
+
+                 int id = resultSet.getInt("ID");
+                 System.out.println("\n================\n");
+                 System.out.println("id: " + id);
+                 System.out.println("Name: " + names);
+                 System.out.println("Director: " + Director);
+                 System.out.println("Year: " + year);
+                 System.out.println("Country: " + Country);
+             }
 
         }
 
+
+        System.out.println("\n");
+        System.out.println("Did you want to look on DB?");
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
+        answer=answer.toLowerCase();
+
+          if (answer.equals("так") || answer.equals("yes")){
+              System.out.println("Okay");
+
+              while (resultSet.next()) {
+
+                  int id = resultSet.getInt("ID");
+                  String name = resultSet.getString("Name");
+                  String Director = resultSet.getString("Director");
+                  int year = resultSet.getInt("Year");
+                  String Country = resultSet.getString("Country");
+
+                  System.out.println("\n================\n");
+                  System.out.println("id: " + id);
+                  System.out.println("Name: " + name);
+                  System.out.println("Director: " + Director);
+                  System.out.println("Year: " + year);
+                  System.out.println("Country: " + Country);
+              }
+          }
+          else {
+              System.out.println("This is end");
+          }
+
+
+        in.close();
+        input.close();
         resultSet.close();
         statement.close();
         connection.close();
     }
 }
-
